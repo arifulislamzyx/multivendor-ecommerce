211dabe10fbbd667e4fe6e8d613bbae512c35de6
arifulislam <arifulislamzyx@gmail.com>
2023-12-10 10:02:09 +0600

    fixed

--- src/Hooks/useProducts.jsx
+++ src/Hooks/useProducts.jsx
@@ -5,16 +5,34 @@ const useProducts = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  // useEffect(() => {
+  //   fetch("https://mitnog-server.vercel.app/products")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       // console.log(data);
+  //       setProducts(data);
+  //       setLoading(false);
+  //     })
+  //     .catch((error) => console.log("Product Fatching Faild for", error));
+  // }, []);
   useEffect(() => {
-    fetch("https://mitnog-server.vercel.app/products")
-      .then((res) => res.json())
-      .then((data) => {
-        // console.log(data);
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://mitnog-server.vercel.app/products");
+        if (!response.ok) {
+          throw new Error('Network response was not ok.');
+        }
+        const data = await response.json();
         setProducts(data);
         setLoading(false);
-      })
-      .catch((error) => console.log("Product Fatching Faild for", error));
+      } catch (error) {
+        console.error("Product Fetching Failed:", error);
+      }
+    };
+  
+    fetchData();
   }, []);
+  
   return { products, loading };
 };
 

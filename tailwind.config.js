ef536158d590e6cd9a4c7b4699b9ab5d61cece22
arifulislam <arifulislamzyx@gmail.com>
2023-12-13 11:23:08 +0600

    redeploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,7 @@ module.exports = {
   important: true,
   
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
